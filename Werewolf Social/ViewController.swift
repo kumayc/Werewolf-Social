56cceb0efc9ce93c4ae4372029376a37dd664a6b
kumayc <little.croco724@gmail.com>
2017-06-23 06:36:58 +1000

    Initial Commit

---
+++ Werewolf Social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Werewolf Social
+//
+//  Created by Chengcheng on 23/6/17.
+//  Copyright © 2017 Chengcheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
