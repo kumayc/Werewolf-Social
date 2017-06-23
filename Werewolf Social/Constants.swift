5e2ad3eaf558d88b9cd4a64f2540b83097caafa8
kumayc <little.croco724@gmail.com>
2017-06-23 19:51:29 +1000

    Sign in ui custom classes complete

---
+++ Werewolf Social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Werewolf Social
+//
+//  Created by Chengcheng on 23/6/17.
+//  Copyright © 2017 Chengcheng. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
